975dbf61ddedc16669a943b7141a0550f613f073
Sergeev1903 <sergeev190391@gmail.com>
2022-04-21 14:10:12 +0300

    Add: design new place view controller

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Артем Сергеев on 21.04.2022.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: Table view delegate
+    
+    // скрываем клавиатуру при нажатии на ячейку
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    // скрываем клавиатуру по нажатию Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
